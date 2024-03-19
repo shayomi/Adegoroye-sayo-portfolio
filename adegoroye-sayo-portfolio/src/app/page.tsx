2808d3993af77f4720305a705710328c4b7c18f3
Sayo Adegoroye <steve.legion94@gmail.com>
2024-03-19 20:48:24 +0100

    update to page.tx

--- adegoroye-sayo-portfolio/src/app/page.tsx
+++ adegoroye-sayo-portfolio/src/app/page.tsx
@@ -6,7 +6,7 @@ import Contact from "@/components/custom/home/Contact";
 
 export default function Home() {
   return (
-    <main className="px-6 pt-24">
+    <main className="px-6 ">
       <Hero />
       <About />
       <Portfolio />
